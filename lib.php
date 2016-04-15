82091bbe0714ca052f9557ac1f6715a830a8d6f6
bashet <bashet01>
2016-04-15 09:42:44 +0100

    Add restriction to moodlemedia icon

--- lib.php
+++ lib.php
@@ -37,21 +37,27 @@ class tinymce_moodlemedia extends editor_tinymce_plugin {
             }
         }
 
-        if ($row = $this->find_button($params, 'moodleemoticon')) {
-            // Add button after 'moodleemoticon' icon.
-            $this->add_button_after($params, $row, 'moodlemedia', 'moodleemoticon');
-        } else if ($row = $this->find_button($params, 'image')) {
-            // Note: We know that the plugin emoticon button has already been added
-            // if it is enabled because this plugin has higher sortorder.
-            // Otherwise add after 'image'.
-            $this->add_button_after($params, $row, 'moodlemedia', 'image');
-        } else {
-            // Add this button in the end of the first row (by default 'image' button should be in the first row).
-            $this->add_button_after($params, 1, 'moodlemedia');
-        }
+        global $COURSE;
+        $context = context_course::instance($COURSE->id);
+        $cat = get_course_category($COURSE->category);
+        if(has_capability('moodle/course:manageactivities', $context) && ($cat->parent == 0) && ($COURSE->id > 1)) {
 
-        // Add JS file, which uses default name.
-        $this->add_js_plugin($params);
+            if ($row = $this->find_button($params, 'moodleemoticon')) {
+                // Add button after 'moodleemoticon' icon.
+                $this->add_button_after($params, $row, 'moodlemedia', 'moodleemoticon');
+            } else if ($row = $this->find_button($params, 'image')) {
+                // Note: We know that the plugin emoticon button has already been added
+                // if it is enabled because this plugin has higher sortorder.
+                // Otherwise add after 'image'.
+                $this->add_button_after($params, $row, 'moodlemedia', 'image');
+            } else {
+                // Add this button in the end of the first row (by default 'image' button should be in the first row).
+                $this->add_button_after($params, 1, 'moodlemedia');
+            }
+
+            // Add JS file, which uses default name.
+            $this->add_js_plugin($params);
+        }
     }
 
     protected function get_sort_order() {
