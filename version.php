567bd1c350b28e672c55b98fa404eab2a608c700
bashet <bashet01>
2016-04-15 09:54:56 +0100

    update version number

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // The current plugin version (Date: YYYYMMDDXX).
-$plugin->version   = 2013110500;
+$plugin->version   = 2016041500;//2013110500;
 // Required Moodle version.
 $plugin->requires  = 2013110500;
 // Full name of the plugin (used for diagnostics).
